0a51e100cc483e665a394ef51eca02af36c90a63
tstuderTOPIC <tstuder@topicdesign.com>
2013-10-11 09:12:45 -0400

    fixed quotation marks in link_to and migration on posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131010174408) do
+ActiveRecord::Schema.define(:version => 20131011122602) do
+
+  create_table "posts", :force => true do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
