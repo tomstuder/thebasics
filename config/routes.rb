40a73d30155a7b88110d796a5c96c2c333e4b36c
tstuderTOPIC <tstuder@topicdesign.com>
2013-10-11 08:32:13 -0400

    cleaned up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Thebasics::Application.routes.draw do
-
+  # root
+  root to: 'home#index'
+  
+  # resources
   resources :posts
 
-
+  # devise
   devise_for :users
 
-  root to: 'home#index'
+  end
 
+# ////////////////////////////////////////////////////////////////
+# ////////////////////////////////////////////////////////////////
+# //////////////////////////////////////////////////////////////// 
+# ////////////////////////////////////////////////////////////////
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -64,4 +71,4 @@ Thebasics::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
+
